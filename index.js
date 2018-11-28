2981bdcb08c8eaac81a17d775e15250a64c43bd3
rac <mayankguptamg.11@gmail.com>
2018-11-28 17:33:26 +0530

    OS

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const cluster=require('cluster');
-const numCPU=require('os').cpus().length;
+const numCPU=require('os');
 const server=require('./server');
 
 const app={};
+
+//console.log(numCPU.constants.signals.SIGKILL);
+var len=numCPU.cpus().length;
+//console.log(numCPU.cpus());
+//console.log(numCPU.endianness());
+console.log(numCPU.freemem())
 app.init=function(){
     if(cluster.isMaster){
-        for(let i=0;i<numCPU;i++){
+        for(let i=0;i<len;i++){
             cluster.fork();
         }
         console.log(numCPU);
